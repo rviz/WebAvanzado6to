50964a3508e7c547cca2749e584a57bd0cc642db
rodrigo <a00829180@itesm.mx>
2024-04-12 16:12:48 -0600

    tarea de tailwind

---
+++ 03-profile/src/App.tsx
@@ -0,0 +1,33 @@
+export default function Example() {
+
+  return (
+    <body className="bg-gray-100 h-screen">
+      <div m-8>
+        <p>&#12288;</p>
+      </div>
+      <div className="max-w-lg mx-auto bg-white rounded-lg shadow-md p-5">
+        <img className="w-32 h-32 rounded-full mx-auto" src="https://crushon.ai/_next/image?url=https%3A%2F%2Fcdn.crushon.ai%2Fimages%2F7b71ea6e-ca3f-491e-8e40-cb43d7065c22%2Fb8533f8c-8005-4912-a23f-5a1f3366ebf6&w=640&q=75" alt="Profile picture"/>
+        <h2 className="text-center text-2xl font-semibold mt-3">Rodrigo V</h2>
+        <p className="text-center text-gray-600 mt-1">Software Engineer</p>
+        <div className="flex justify-center mt-5">
+          <a href="https://github.com/rviz" className="text-blue-500 hover:text-blue-700 mx-3">GitHub</a>
+        </div>
+        <div className="mt-5">
+          <h3 className="text-xl font-semibold">Bio</h3>
+          <p className="text-gray-600 mt-2 mb-6">Currently studying Computer Science and Engineering @TecMty. My favorite fields are Cybersecurity, UI/UX Design, and Web Development.</p>
+          <h3 className="text-xl font-semibold">Digital Skills</h3>
+          <span className="mr-2 text-sm font-medium bg-gray-100 py-1 px-2 rounded text-gray-500 align-middle">OOP</span>
+          <span className="mr-2 text-sm font-medium bg-cyan-100 py-1 px-2 rounded text-cyan-500 align-middle">Web Development</span>
+          <span className="mr-2 text-sm font-medium bg-blue-100 py-1 px-2 rounded text-blue-500 align-middle">Cybersecurity</span>
+          <span className="mr-2 text-sm font-medium bg-orange-100 py-1 px-2 rounded text-orange-500 align-middle">Swift</span>
+          <br className="m-2" />
+          <span className="mr-2 text-sm font-medium bg-red-100 py-1 px-2 rounded text-red-500 align-middle">C++</span>
+          <span className="mr-2 text-sm font-medium bg-green-100 py-1 px-2 rounded text-green-500 align-middle">Python</span>
+          <span className="mr-2 text-sm font-medium bg-yellow-100 py-1 px-2 rounded text-yellow-500 align-middle">JavaScript</span>
+          <span className="mr-2 text-sm font-medium bg-gray-100 py-1 px-2 rounded text-gray-500 align-middle">Linux</span>
+          <span className="mr-2 text-sm font-medium bg-orange-100 py-1 px-2 rounded text-orange-500 align-middle">Git</span>
+        </div>
+      </div>
+    </body>
+  )
+}
